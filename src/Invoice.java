40cd3cfd85a279d573378bf27289029936442725
maviva97@gmail.com <maviva97@gmail.com>
2023-10-12 13:21:29 +0300

    l

--- src/Invoice.java
+++ src/Invoice.java
@@ -4,34 +4,54 @@ public class Invoice {
     private  int Cantitatea;
     private  Double Pret;
     private  Double Amount;
-    public void setModel(Modell model) {
-
-        this.Model = model;
-    }
-    public void setDescriere(String descriere) {
-        this.Descriere = descriere;
-    }
-    public void setCantitatea(int cantitatea) {
-        if (Cantitatea<0) this.Cantitatea=0;
-        else this.Cantitatea = cantitatea;
-    }
-    public void setPret(Double pret) {
-        if (pret<0) this.Pret= 0.0;
-        else this.Pret = pret;
-    }
-    public Modell getModel() {
-        return Model;
-    }
     public String getDescriere() {
+
         return Descriere;
     }
     public int getCantitatea() {
+
         return Cantitatea;
     }
     public Double getPret() {
         return Pret;
     }
+    public void setModel(Modell model) {
+
+        this.Model = model;
+    }
+    public void setDescriere(String descriere) {
+
+        this.Descriere = descriere;
+    }
+
+    public void setCantitatea(int cantitatea) {
+        if (cantitatea>0) {
+            this.Cantitatea = cantitatea;
+        }
+        else {
+            this.Cantitatea=0;
+            System.out.println("Cantitatea nu este setata ");
+
+        }
+    }
+    public void setPret(Double pret) {
+        if (pret>0) {this.Pret = pret;
+        }   else {
+      this.Pret =0.0;
+        System.out.println("Pretul este gresit ");
+        }
+    }
+    public Modell getModel() {
+        return Model;
+    }
+
 public  Double getAmount(){
 return this.Amount=Pret*Cantitatea;
+    }
+    public  void Afisare(){
+    System.out.println("Aveti in cos "+ getModel()+"  "+getDescriere());
+    System.out.println("La pretul "+ getPret());
+    System.out.println("Cantitatea "+  getCantitatea());
+    System.out.println("Suma totala  "+ getAmount()+" lei");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ public class Main {
       invoice.setModel(Modell.Siemens);
       invoice.setDescriere("Masina de spalat A+++");
       invoice.setCantitatea(9);
-      invoice.setPret(14000.00);
-        System.out.println("Aveti in cos "+ invoice.getModel()
-                +"  "+invoice.getDescriere()+
-               " Cantitatea "+  invoice.getCantitatea()
-                +" Suma totala  "+ invoice.getAmount()+" lei");
+      invoice.setPret(-4000.00);
+       invoice.Afisare();
     }
 }
